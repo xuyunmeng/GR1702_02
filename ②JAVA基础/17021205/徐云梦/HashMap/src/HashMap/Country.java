831b29117e83b95ed25b8fd3f521c31711ad25ce
xuyunmeng <34231497+xuyunmeng@users.noreply.github.com>
2017-12-05 10:26:17 +0800

    1702
    1205

---
+++ ②JAVA基础/17021205/徐云梦/HashMap/src/HashMap/Country.java
@@ -0,0 +1,23 @@
+package HashMap;
+
+public class Country {
+
+		String name;
+		long population;
+		
+		public Country(String name, long population) {
+			super();
+			this.name = name;
+			this.population = population;
+		}
+
+		@Override
+		public String toString() {
+			return "Country [name=" + name + ", population=" + population + "]";
+		}
+
+		
+		
+
+
+}
